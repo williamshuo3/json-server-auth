ea1bf041ac5719559040c3d61ccd8de97e9cbbb9
williamshuo3 <121234908+williamshuo3@users.noreply.github.com>
2023-11-27 16:27:24 +0800

    Update login.js

--- login.js
+++ login.js
@@ -3,25 +3,34 @@
 const inputEmail = document.querySelector('#inputEmail');
 const inputPassword = document.querySelector('#inputPassword');
 const loginBtn = document.querySelector('#login');
-
+console.log(loginBtn)
 let token = "";
-loginBtn.addEventListener("click",function(){
-    if(inputEmail.value.trim() == "" || inputPassword.value.trim() == ""){
+loginBtn.addEventListener("click", function(){
+    console.log("已點擊")
+
+    if(
+    inputEmail.value.trim() == "" || 
+    inputPassword.value.trim() == ""
+    ){
         alert("請填寫帳號/密碼")
         return;
     };
     let obj = {};        
         obj.email = inputEmail.value;
         obj.password = inputPassword.value;
-        login(obj);
+        // login(obj);
 })
 
 function login(obj){
-    axios.post('https://json-server-auth-q2r9.onrender.com/users', obj)
+    axios.post('https://json-server-auth-q2r9.onrender.com/login', {
+        "email":"2@333",
+        "password":"222222"
+    })
     .then(function(res){
         if (res.status == 200) {
-            window.location.assign("index.html");
-          }else{
+            // window.location.assign("index.html");
+         console.log(res) 
+        }else{
             alert("Login failed. Please check your credentials.")
           }
     })
@@ -29,7 +38,3 @@ function login(obj){
         console.log(error.response);
     })
 }
-// login()
-// wwww@ffff
-// wwwwww
-// {'email':'rrrrr@eeeee','password':'qqqqqq'}
